7ad66acbbcc232683ee83bb1ee502a77d0a309d4
lorenzotade <lorenzo.tade@gmail.com>
2021-05-03 16:09:27 +0200

    terminato

--- jsnack-12/script.js
+++ jsnack-12/script.js
@@ -1,11 +1,31 @@
 let browserWindow = {
-  tabs: ['GitHub','Facebook', 'PornHub', 'Famiglia Cristiana'],
+  tabs: ['GitHub', 'Facebook', 'PornHub', 'Famiglia Cristiana'],
   activeTab: 1
 }
 
 const socials = ['Facebook', 'Instagram', 'Twitter', 'Parler', 'LinkedIn'];
 
-browserWindow.tabs.forEach((tab) => {
+let {tabs, activeTab} = browserWindow;
+
+let filteredTabs = tabs.filter((tab) => {
+  if (!socials.includes(tab)) {
+    return true;
+  } else {
+    return false;
+  }
+})
+
+browserWindow.tabs = filteredTabs;
+if (activeTab > (filteredTabs.length - 1)) {
+  browserWindow.activeTab = activeTab - 1;
+}
+
+console.log(browserWindow)
+
+
+// ROBE BRUTTE
+
+/* browserWindow.tabs.forEach((tab) => {
   let {tabs, activeTab} = browserWindow;
   console.log(tab);
   let socialTab = socials.filter((social) => tab === social);
@@ -14,8 +34,4 @@ browserWindow.tabs.forEach((tab) => {
   console.log(socialIndex);
   tabs.splice(socialIndex, 1);
   console.log(tabs);
-  /*if (activeTab === socialIndex) {
-    activeTab++;
-  } */
-});
-console.log(browserWindow)
+}); */
